2ef13344c84f6e441b462fa38d347c83f45279b0
livemehere <rhdxoals@gmail.com>
2023-01-14 03:54:59 +0900

    done

--- App.tsx
+++ App.tsx
@@ -1,7 +1,13 @@
 import cn from "./src";
+import './style.css'
+import {useState} from "react";
 
 function App(){
-    return <div className={cn('hi','red')}>App</div>
+    const [active, setActive] = useState(false);
+    return <div className={cn('hi','red', {active:'1'})}>
+        <h1>HIHIH</h1>
+        <button onClick={()=> setActive(prev => !prev)}>click</button>
+    </div>
 }
 
 export default App;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
-function cn(...args){
-    console.log(args)
-    return 'AA'
+/**
+ * @param args List up classNames as strings and objects
+ */
+function cn(...args :any[]){
+    args = args.map(s=> {
+        if(typeof s === 'object'){
+            return Object.keys(s).map(k=> !!s[k] ? k : undefined)
+        }
+        return s;
+    })
+    return args.flat().filter(s=> s).join(' ')
 }
 
 export default cn;
