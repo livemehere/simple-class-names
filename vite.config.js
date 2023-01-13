901272c31c139c27f110f84498e7b789b9fca0ae
livemehere <rhdxoals@gmail.com>
2023-01-14 04:15:29 +0900

    publish setting

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from "vite";
 import react from '@vitejs/plugin-react'
+import {join} from 'path'
 
 export default defineConfig({
     plugins:[react()],
     build:{
-        outDir:'dist',
-
+        lib:{
+            entry: join(__dirname, 'src/index.ts'),
+            name:'simple-class-names',
+            fileName:'index'
+        }
     }
 })
